62c7897051d333676b5cf103a5b382103040074e
Sebastian Szuch <98364552+sjszuch@users.noreply.github.com>
2023-06-26 16:02:16 -0400

    Added slice conversion

--- FE/src/app/components/splash/splash.component.ts
+++ FE/src/app/components/splash/splash.component.ts
@@ -14,16 +14,16 @@ export class SplashComponent {
   startTime!: string;
 
   times = [
-    {value: 'eight-am', viewValue: '8am'},
-    {value: 'nine-am', viewValue: '9am'},
-    {value: 'ten-am', viewValue: '10am'},
-    {value: 'eleven-am', viewValue: '11am'},
-    {value: 'twelve-pm', viewValue: '12pm'},
-    {value: 'one-pm', viewValue: '1pm'},
-    {value: 'two-pm', viewValue: '2pm'},
-    {value: 'three-pm', viewValue: '3pm'},
-    {value: 'four-pm', viewValue: '4pm'},
-    {value: 'five-pm', viewValue: '5pm'}
+    {value: '8', viewValue: '8am'},
+    {value: '9', viewValue: '9am'},
+    {value: '10', viewValue: '10am'},
+    {value: '11', viewValue: '11am'},
+    {value: '12', viewValue: '12pm'},
+    {value: '1', viewValue: '1pm'},
+    {value: '2', viewValue: '2pm'},
+    {value: '3', viewValue: '3pm'},
+    {value: '4', viewValue: '4pm'},
+    {value: '5', viewValue: '5pm'}
   ];
 
 
@@ -43,7 +43,7 @@ export class SplashComponent {
   startDate = new Date();
   name!: string;
   date!: string;
-  length!: string;
+  length!: number;
 
 
 
@@ -53,15 +53,17 @@ export class SplashComponent {
 
     // Display the name, date, time, and length fields in the console
     console.log(this.name);
-    console.log(this.date)
-    console.log(this.startTime)
-    console.log(this.length)
+    console.log(this.date);
+    console.log(this.startTime);
+    console.log(this.length);
+    console.log("Slices:" + (this.length * 60));
 
     // If any of the fields are empty, display an error message
     if (this.name == null || this.date == null || this.startTime == null || this.length == null) {
       this._snackBar.open("Please fill out all fields", "Okay");
     }
-    
+
+
 
 
 
